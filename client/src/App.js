9056b339fa780664362b9a25e622b8000badb96b
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-01-08 00:41:16 +0530

    setup frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import * as React from 'react';
+import Map from "react-map-gl";
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
